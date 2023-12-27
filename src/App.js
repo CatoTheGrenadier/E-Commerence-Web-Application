d55a536b99d4e1c7664d5dd70bab88937d867de3
CatoTheGrenadier <freeman123459@outlook.com>
2023-12-27 12:49:54 -0500

    88

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
+import Navigation from './routes/navgation/navigation.component'
 import Home from "./routes/home/home.component"
+import SignIn from './sign-in/sign-in.component'
+
 const Shop = () => {
   return (
     <h1>
@@ -7,11 +10,15 @@ const Shop = () => {
     </h1>
   )
 }
+
+
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home />} >
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
