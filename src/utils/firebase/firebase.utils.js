b5f51d8dbf010912925b2b2868a7625366f9d110
CatoTheGrenadier <freeman123459@outlook.com>
2024-01-01 14:39:30 -0500

    95

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,13 @@ import {
     GoogleAuthProvider,
 } from 'firebase/auth';
 
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+} from 'firebase/firestore'
+
 const firebaseConfig = {
   apiKey: "AIzaSyBZStFWyZ8DWUz8FBYNZdXBzRA3GCfbDmI",
   authDomain: "crwn-clothing-db-be66b.firebaseapp.com",
@@ -25,3 +32,32 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef)
+
+    const userSnapshot = await getDoc(userDocRef)
+    console.log(userSnapshot)
+    console.log(userSnapshot.exists())
+
+    if (!userSnapshot.exists()){
+        const { displayName, email } = userAuth;
+        const createAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createAt
+            });
+        } catch (error){
+            console.log('error creating the user', error.message);
+        }
+    }
+
+    return userDocRef;
+}
