700c10a6f48a8234bda9c6f9d0d29d5b4d7b6998
CatoTheGrenadier <freeman123459@outlook.com>
2024-01-04 15:27:03 -0500

    116 cart icon & cart dropdown

--- src/routes/navgation/navigation.component.jsx
+++ src/routes/navgation/navigation.component.jsx
@@ -3,10 +3,14 @@ import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/87 - crown.svg'
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import { CartContext } from '../../contexts/cart.context';
 import './navigation.styles.scss'
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext)
+  const { isCartOpen } = useContext(CartContext)
 
   return (
     <Fragment>
@@ -18,6 +22,7 @@ const Navigation = () => {
           <Link className='nav-link' to={'/shop'}>
               SHOP   
           </Link>
+
           {currentUser ? (
             <span className='nav-link' onClick={signOutUser}>
               {' '}
@@ -28,7 +33,10 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown/>}
       </div>
       <Outlet/>
     </Fragment>
